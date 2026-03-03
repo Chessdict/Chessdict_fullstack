e25f82cefe456f5d715f6d94d95d4fc0938f6e5a
romeoscript <romeobourne211@gmail.com>
2026-03-03 06:49:32 +0100

    port

--- server.mjs
+++ server.mjs
@@ -5,8 +5,8 @@ import { Server } from "socket.io";
 import { PrismaClient } from "@prisma/client";
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
-const port = 3000;
+const hostname = "0.0.0.0";
+const port = process.env.PORT || 3000;
 
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
